f768d81d66a51ad0ae6fd46167465ed1a921c632
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-01-12 08:53:42 +0700

    add collection roles

---
+++ model/role.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const roleSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        permissions: {
+            type: Array,
+            default: []
+        },
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deletedAt: Date,
+
+    },
+    { timestamps: true }
+)
+
+const Role = mongoose.model('role', roleSchema, 'roles')
+
+module.exports = Role
