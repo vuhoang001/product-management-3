a729a65d0a3211328e70df26995feccaf07cb399
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-01-18 22:39:08 +0700

    Phân quyền cho slider

--- middlewares/admin/authMiddleware.middleware.js
+++ middlewares/admin/authMiddleware.middleware.js
@@ -1,5 +1,6 @@
 const system = require('../../config/system')
 const Accout = require('../../model/accounts.model')
+const Role = require('../../model/role.model')
 module.exports.requireAuth = async (req, res, next) => {
 
     if (!req.cookies.token) {
@@ -8,11 +9,13 @@ module.exports.requireAuth = async (req, res, next) => {
     }
 
     const user = await Accout.findOne({ token: req.cookies.token })
-
+    const role = await Role.findOne({ _id: user.role_id }).select("title permissions")
     if (req.cookies.token != user.token) {
         res.redirect(`/${system.admin_path}/dashboard`)
         return;
     }
 
+    res.locals.user = user
+    res.locals.role = role
     next()
 }
