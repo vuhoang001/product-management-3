e08cdd2e9afa07c09e2c7c6aed1422f9fec70895
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-01-18 14:05:05 +0700

    Build log out

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,10 +1,12 @@
 const Account = require('../../model/accounts.model')
 const systemConfig = require('../../config/system')
 const md5 = require('md5')
+//[GET] admin/auth/login
 module.exports.login = (req, res) => {
     res.render('admin/pages/auth/login.pug')
 }
 
+//[POST] admin/auth/loginPost
 module.exports.loginPost = async (req, res) => {
     const email = req.body.email
     const password = req.body.password
@@ -13,8 +15,17 @@ module.exports.loginPost = async (req, res) => {
         if (user.password == md5(password)) {
             res.cookie("token", user.token)
             res.redirect(`/${systemConfig.admin_path}/dashboard`)
+        } else {
+            res.redirect('back')
         }
     } else {
         res.redirect('back')
     }
+}
+
+
+// [GET] admin/auth/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("token")
+    res.redirect(`/${systemConfig.admin_path}/auth/login`)
 }
--- routes/admin/auth.router.js
+++ routes/admin/auth.router.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('../../controllers/admin/auth.controller.js')
 router.get('/login', controller.login)
 router.post('/login', controller.loginPost)
+router.get('/logout', controller.logout)
 
 module.exports = router
