251e8bbbf7d9c0908d2d2abc6edaa27b6ff32d35
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-01-23 01:02:31 +0700

    Trang danh sách sản phẩm bên client (lấy ra các sản phẩm ở danh mục con)con

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,6 +1,6 @@
 const products = require("../../model/products.model")
 const productsHelper = require('../../helpers/products.helper')
-
+const productCategoryModel = require('../../model/products-category.model')
 // [GET] /products 
 module.exports.index = async (req, res) => {
 
@@ -23,4 +23,49 @@ module.exports.detail = async (req, res) => {
     res.render("client/pages/products/detail.pug", {
         products: Product
     })
+}
+
+//[GET] products/:categoryProduct
+module.exports.category = async (req, res) => {
+    const slug = req.params.categoryProduct
+
+    const category = await productCategoryModel.findOne({
+        deleted: false,
+        slug: slug
+    })
+
+    const getSubCategory = async (parentID) => {
+        const subs = await productCategoryModel.find({
+            status: "active",
+            deleted: false,
+            parent_id: parentID
+        })
+
+        let allSubs = [...subs]
+
+        for (const sub of subs) {
+            const childs = await getSubCategory(sub.id)
+            allSubs = allSubs.concat(childs)
+        }
+
+        return allSubs
+    }
+
+    const listSubCategory = await getSubCategory(category.id)
+
+    const listSubCategoryID = listSubCategory.map(item => item.id)
+
+    const Product = await products.find({
+        deleted: false,
+        status: "active",
+        parent_id: { $in: [category.id, ...listSubCategoryID] }
+    }).sort({ position: 'desc' })
+
+    const newProducts = productsHelper.price(Product)
+
+    res.render("client/pages/products/index", {
+        pageTitle: category.title,
+        products: newProducts
+    })
+
 }
--- routes/client/products.route.js
+++ routes/client/products.route.js
@@ -4,4 +4,5 @@ const productsController = require("../../controllers/client/products.controller
 
 router.get("/", productsController.index)
 router.get("/detail/:slug", productsController.detail)
+router.get("/:categoryProduct", productsController.category)
 module.exports = router
